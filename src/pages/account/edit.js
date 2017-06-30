53705b8692d95a890b5251b2d3ed8c85f384d9ae
h2rmone <3357458@qq.com>
2017-06-30 21:39:48 +0800

    :construction: add form in edit page

--- src/pages/account/edit.js
+++ src/pages/account/edit.js
@@ -1,4 +1,7 @@
 import React, { PureComponent } from 'react'
+import AccountForm from './form'
+import AccountTabs from './tabs'
+import { Button } from 'antd'
 
 export default class EditAccount extends PureComponent {
 
@@ -6,9 +9,27 @@ export default class EditAccount extends PureComponent {
     console.log(this.props.location.state)
   }
 
+  handleSave () {
+
+  }
+
   render () {
+    const { id, user_name, login_name, phone, email } = this.props.location.state
     return (
       <div className="page_account-edit">
+        <AccountForm
+          id={id}
+          userName={user_name}
+          loginName={login_name}
+          phone={phone}
+          email={email}
+        >
+        </AccountForm>
+        <Button onClick={this.handleSave}>
+          保存
+        </Button>
+        <AccountTabs>
+        </AccountTabs>
       </div>
     )
   }
