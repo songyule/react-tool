a950ced7f4dd202d08a4388c8ca023d1b5af7ba0
h2rmone <3357458@qq.com>
2017-06-30 21:42:07 +0800

    :truck: remove account in base router

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,6 @@ import main from 'pages/main/index'
 import notFound from 'pages/404/index'
 import Login from 'pages/login/index'
 import Goods from 'pages/commodity/index'
-import AccountList from 'pages/account/index'
-import AccountEdit from 'pages/account/edit'
 
 import {
   BrowserRouter as Router,
@@ -24,8 +22,6 @@ const routes =  () => (
         <Route path='/login' component={Login} />
         <Route path='/main' component={main} />
         <Route path='/goods' component={Goods} />
-        <Route path='/account-list' component={AccountList} />
-        <Route path='/account-edit' component={AccountEdit} />
         <Route path='*' component={notFound} />
       </Switch>
     </div>
