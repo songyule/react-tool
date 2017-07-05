b608a5af6135006cdd6804a867f7f44a7dfd9af9
h2rmone <3357458@qq.com>
2017-07-05 15:45:33 +0800

    :sparkles: add attributes

--- src/pages/attributes/index.js
+++ src/pages/attributes/index.js
@@ -1,29 +1,69 @@
 import React, { PureComponent } from 'react'
-import AttributesTree from './attributes'
-import data from './attr'
-import Title from 'components/title'
+import style from './index.css'
+import treeData from './attr'
 import arrayToTree from 'array-to-tree'
+import Title from 'components/title'
+import { Tree, Button } from 'antd'
+const [ TreeNode, ButtonGroup ] = [ Tree.TreeNode, Button.Group ]
 
-class Attributes extends PureComponent {
-  componentWillMount () {
-    this.getData()
+export default class Attributes extends PureComponent {
+  constructor () {
+    super()
+
+    this.state = {
+      expandedKeys: []
+    }
+  }
+  onSelect = (expandedKeys, info) => {
+    console.log('selected', expandedKeys, info)
+    this.setState({ expandedKeys })
+  }
+  
+  handelAdd (e) {
+    e.stopPropagation()
   }
 
-  getData () {
-    this.setState({ data: [arrayToTree(data.data.attribute)]})
-  }
+  render() {
+    const { expandedKeys } = this.state
 
-  render () {
-    const { data } = this.state
+    const titleItem = (title, canDelete) => {
+      return (
+        <div className={style['tree__title']}>
+          <span>{title}</span>
+          <ButtonGroup>
+            <Button size="small" onClick={(e) => this.handelAdd(e) }> 添加 </Button>
+            <Button size="small" type="primary"> 编辑 </Button>
+            {canDelete ? <Button size="small"> 删除 </Button> : null}
+          </ButtonGroup>
+        </div>
+      )
+    }
+
+    const loop = data => data.map((item) => {
+     if (item.children) {
+       return (
+         <TreeNode
+           key={item.id}
+           title={titleItem(item.name_cn, false)}
+         >
+           {loop(item.children)}
+         </TreeNode>
+       )
+     }
+     return <TreeNode key={item.id} title={titleItem(item.name_cn, true)} />
+    })
+
     return (
       <div>
-        <Title title="商城通用类目">
-        </Title>
-        <AttributesTree treeData={data}>
-        </AttributesTree>
+        <Title title="属性管理" />
+        <Tree
+          autoExpandParent
+          expandedKeys={expandedKeys}
+          onSelect={this.onSelect}
+        >
+          {loop(arrayToTree(treeData.data.attribute))}
+        </Tree>
       </div>
     )
   }
 }
-
-export default Attributes
