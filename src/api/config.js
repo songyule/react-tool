3139d36a9215a922e304b8bf5daf623267a6b95b
ws456999 <ruiru5921@sina.com>
2017-07-11 19:11:56 +0800

    :bug: change commodity load

--- src/api/config.js
+++ src/api/config.js
@@ -2,7 +2,7 @@ import { IS_DEV } from '../config'
 
 export const API_ROOT = IS_DEV ? 'https://test.fuliaoyi.com/tool/api' : 'https://tool.fuliaoyi.com/api'
 
-export const QINIU_API = IS_DEV ? 'https://up.qbox.me' : 'http://upload.qiniu.com'
+export const QINIU_API = IS_DEV ? 'http://upload.qiniu.com' : 'https://up.qbox.me'
 
 export const QINIU_PREFIX = IS_DEV ? 'https://timage.fuliaoyi.com/' : 'https://image.fuliaoyi.com/'
 // export const API_ROOT = IS_DEV ? 'https://test.fuliaoyi.com/mrp/api' : 'https://tool.fuliaoyi.com/api'
