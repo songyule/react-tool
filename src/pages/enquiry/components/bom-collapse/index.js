9a115804cda7711cfe78e6f8c6c37f3858becd7c
kokoro <8462304@qq.com>
2017-08-01 21:57:46 +0800

    :bug: add offer data filter

--- src/pages/enquiry/components/bom-collapse/index.js
+++ src/pages/enquiry/components/bom-collapse/index.js
@@ -15,7 +15,7 @@ export default class BomCollapse extends PureComponent {
   }
 
   render () {
-    const { orgList, material } = this.props
+    const { orgList, materials } = this.props
 
     const formItemLayout = {
       labelCol: { span: 6 },
@@ -93,57 +93,59 @@ export default class BomCollapse extends PureComponent {
     return (
       <Collapse>
         <Panel header="展开物料详情" key="1">
-          <Card title="物料" style={{ marginBottom: '15px' }}>
-            {materialItemConfig.map(config => (
-              <Col key={config.valid} span={12}>
-                <FormItem {...formItemLayout} label={config.label}>
-                  <Input disabled value={material[config.valid]} />
+          { materials.map(material => (
+            <Card title="物料" style={{ marginBottom: '15px' }}>
+              {materialItemConfig.map(config => (
+                <Col key={config.valid} span={12}>
+                  <FormItem {...formItemLayout} label={config.label}>
+                    <Input disabled value={material[config.valid]} />
+                  </FormItem>
+                </Col>
+              ))}
+              <Col span={12}>
+                <FormItem {...formItemLayout} label="包含运费">
+                  <RadioGroup value={material.include_express_fee} disabled>
+                    <Radio value={1}> 包含运费 </Radio>
+                    <Radio value={0}> 不包含运费 </Radio>
+                  </RadioGroup>
                 </FormItem>
               </Col>
-            ))}
-            <Col span={12}>
-              <FormItem {...formItemLayout} label="包含运费">
-                <RadioGroup value={material.include_express_fee} disabled>
-                  <Radio value={1}> 包含运费 </Radio>
-                  <Radio value={0}> 不包含运费 </Radio>
-                </RadioGroup>
-              </FormItem>
-            </Col>
-            <Col span={12}>
-              <FormItem {...formItemLayout} label="供应商选择">
-                <Select placeholder="请选择" value={material.supplier_id} disabled>
-                  {
-                    orgList.map(item => (
-                      <Option key={item.id} value={item.id}> {item.name_official || item.name_cn} </Option>
-                    ))
-                  }
-                </Select>
-              </FormItem>
-            </Col>
-            {materialItemWithUnitConfig.map(config => (
-              <Col key={config.valid} span={12}>
-                <FormItem {...formItemLayout} label={config.label}>
-                  <Input
-                    disabled
-                    addonAfter={config.unit && <span>{config.unit}</span>}
-                    value={material[config.valid]}
-                  />
+              <Col span={12}>
+                <FormItem {...formItemLayout} label="供应商选择">
+                  <Select placeholder="请选择" value={material.supplier_id} disabled>
+                    {
+                      orgList.map(item => (
+                        <Option key={item.id} value={item.id}> {item.name_official || item.name_cn} </Option>
+                      ))
+                    }
+                  </Select>
                 </FormItem>
               </Col>
-            ))}
-            {materialItemFullColConfig.map(config => (
-              <Col key={config.valid} span={24}>
-                <FormItem {...formItemFullColLayout} label={config.label}>
-                  <Input disabled />
+              {materialItemWithUnitConfig.map(config => (
+                <Col key={config.valid} span={12}>
+                  <FormItem {...formItemLayout} label={config.label}>
+                    <Input
+                      disabled
+                      addonAfter={config.unit && <span>{config.unit}</span>}
+                      value={material[config.valid]}
+                    />
+                  </FormItem>
+                </Col>
+              ))}
+              {materialItemFullColConfig.map(config => (
+                <Col key={config.valid} span={24}>
+                  <FormItem {...formItemFullColLayout} label={config.label}>
+                    <Input disabled />
+                  </FormItem>
+                </Col>
+              ))}
+              <Col span={24}>
+                <FormItem {...formItemFullColLayout} label="备注">
+                  <TextArea rows={4} value={material.comment} disabled/>
                 </FormItem>
               </Col>
-            ))}
-            <Col span={24}>
-              <FormItem {...formItemFullColLayout} label="备注">
-                <TextArea rows={4} value={material.comment} disabled/>
-              </FormItem>
-            </Col>
-          </Card>
+            </Card>
+          )) }
         </Panel>
       </Collapse>
     )
