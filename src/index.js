470f45d4b816ba4798baf44b12e5d982586af432
kokoro <8462304@qq.com>
2017-08-07 12:08:10 +0800

    :bug: add config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import registerServiceWorker from './utils/registerServiceWorker'
+// import registerServiceWorker from './utils/registerServiceWorker'
 import { Provider } from 'react-redux'
 import store from '@/redux/store'
 import history from 'router/history'
@@ -15,4 +15,4 @@ render(
   </Provider>,
   document.getElementById('root'))
 
-registerServiceWorker()
+// registerServiceWorker()
